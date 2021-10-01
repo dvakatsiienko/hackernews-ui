7bef4035b1fa23ef072eba196fc5644eb6108b30
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2021-10-02 00:17:26 +0300

    feat(core): improve authentication protection

--- src/lib/apollo/links/authLink.ts
+++ src/lib/apollo/links/authLink.ts
@@ -4,12 +4,14 @@ import { setContext } from '@apollo/client/link/context';
 const AUTH_TOKEN_NAME = process.env.NEXT_PUBLIC_AUTH_TOKEN_NAME;
 
 export const authLink = setContext((_, prevCtx) => {
-    const token = process.browser ? localStorage.getItem(AUTH_TOKEN_NAME) : '';
+    const token = process.browser
+        ? localStorage.getItem(AUTH_TOKEN_NAME)
+        : null;
 
     const ctx = {
         headers: {
             ...prevCtx.headers,
-            Authorization: `Bearer ${token}`,
+            Authorization: token ? `Bearer ${token}` : '',
         },
     };
 
