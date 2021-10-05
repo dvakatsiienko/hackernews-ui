17cc078db9697968b7a410ddf20bdfa2444a0347
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2021-10-05 19:31:32 +0300

    fix(types): temporary mute unused types

--- src/components/SearchPostForm/resolver.ts
+++ src/components/SearchPostForm/resolver.ts
@@ -5,10 +5,10 @@ import { yupResolver } from '@hookform/resolvers/yup';
 /* Instruments */
 import * as gql from '@/graphql';
 
-const schema: yup.SchemaOf<gql.FeedQueryVariables> = yup.object({
+const schema: yup.SchemaOf<FormShape> = yup.object({
     filter: yup.string(),
-    skip:   yup.number(),
-    take:   yup.number(),
 });
 
 export const resolver = yupResolver(schema);
+
+type FormShape = Pick<gql.FeedQueryVariables, 'filter'>;
