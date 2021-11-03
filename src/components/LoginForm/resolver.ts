64ef268e02c203db228d6ce3858d2851b1b34413
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2021-11-04 00:17:14 +0200

    fix(core): align import shape in compliance with next.js 12

--- src/components/LoginForm/resolver.ts
+++ src/components/LoginForm/resolver.ts
@@ -1,6 +1,6 @@
 /* Core */
 import * as yup from 'yup';
-import { yupResolver } from '@hookform/resolvers/yup';
+import { yupResolver } from '@hookform/resolvers/yup/dist/yup';
 
 /* Instruments */
 import * as gql from '@/graphql';
